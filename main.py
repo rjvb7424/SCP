44064b9222de7f99d6da04c288b4fdb9ec76582f
rjvb7424 <rjvb7424@leeds.ac.uk>
2025-11-19 22:02:00 +0000

    Operation now has a basic system to generate code names from a list of adjectives and nouns

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from anomaly import Anomaly
+from operation.operation import Operation
 
 anomaly = Anomaly()
 print(anomaly.get_description())
+
+operation = Operation()
+print(operation.get_code_name())
