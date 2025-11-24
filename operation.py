36207c8559716608f4b33ecb3d4fd747eddd04cb
rjvb7424 <rjvb7424@leeds.ac.uk>
2025-11-24 20:51:10 +0000

    Changed data to cn_data to reflect other json files I would like to add in the future

--- operation.py
+++ operation.py
@@ -3,10 +3,10 @@ import json
 
 class Operation:
     with open("code_names.json", "r") as f:
-        data = json.load(f)
+        cn_data = json.load(f)
 
     def __init__(self):
-        self.code_name = random.choice(self.data["adjectives"]) + " " + random.choice(self.data["nouns"])
+        self.code_name = random.choice(self.cn_data["adjectives"]) + " " + random.choice(self.cn_data["nouns"])
 
     def __repr__(self):
         return f"{self.code_name}"
