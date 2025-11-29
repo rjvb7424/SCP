9ec774f7d95c061deb85d44d4d9d041906295715
rjvb7424 <rjvb7424@leeds.ac.uk>
2025-11-29 15:57:33 +0000

    Personnel attribute generation now follows a normal distribution so its not fully random

--- personnel.py
+++ personnel.py
@@ -5,33 +5,46 @@ class Personnel:
     with open("names.json", "r") as f:
         _data = json.load(f)
 
+    @staticmethod
+    def _generate_attribute(mu=10, sigma=4, lo=0, hi=20):
+        """Return an int drawn from a normal distribution"""
+        value = random.gauss(mu, sigma)
+        value = round(value)
+        value = max(lo, min(hi, value))
+        return int(value)
+
     def __init__(self):
         # personnel identity
         self.gender = random.choice(["male", "female"])
         self.fname = random.choice(self._data["first_names"][self.gender])
         self.lname = random.choice(self._data["last_names"])
-        # personnel attributes
+
         self.attributes = {}
+
         # administrative attributes
-        self.attributes["leadership"] = random.randint(0, 20)
-        self.attributes["logistics"] = random.randint(0, 20)
-        self.attributes["planning"] = random.randint(0, 20)
+        self.attributes["leadership"] = self._generate_attribute()
+        self.attributes["logistics"] = self._generate_attribute()
+        self.attributes["planning"] = self._generate_attribute()
+
         # combat attributes
-        self.attributes["marksmanship"] = random.randint(0, 20)
-        self.attributes["situational_awareness"] = random.randint(0, 20)
-        self.attributes["physical_fitness"] = random.randint(0, 20)
+        self.attributes["marksmanship"] = self._generate_attribute()
+        self.attributes["situational_awareness"] = self._generate_attribute()
+        self.attributes["physical_fitness"] = self._generate_attribute()
+
         # research attributes
-        self.attributes["data_collection"] = random.randint(0, 20)
-        self.attributes["anomaly_knowledge"] = random.randint(0, 20)
-        self.attributes["analytical_thinking"] = random.randint(0, 20)
+        self.attributes["data_collection"] = self._generate_attribute()
+        self.attributes["anomaly_knowledge"] = self._generate_attribute()
+        self.attributes["analytical_thinking"] = self._generate_attribute()
+
         # mental attributes
-        self.attributes["adaptability"] = random.randint(0, 20)
-        self.attributes["determination"] = random.randint(0, 20)
-        self.attributes["negotiation"] = random.randint(0, 20)
+        self.attributes["adaptability"] = self._generate_attribute()
+        self.attributes["determination"] = self._generate_attribute()
+        self.attributes["negotiation"] = self._generate_attribute()
+
         # medical attributes
-        self.attributes["surgery"] = random.randint(0, 20)
-        self.attributes["psychology"] = random.randint(0, 20)
-        self.attributes["first_aid"] = random.randint(0, 20)
+        self.attributes["surgery"] = self._generate_attribute()
+        self.attributes["psychology"] = self._generate_attribute()
+        self.attributes["first_aid"] = self._generate_attribute()
 
     def __repr__(self):
         return f"{self.fname} {self.lname} ({self.gender})"
