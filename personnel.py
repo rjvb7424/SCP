7f47290ff20ede3413fdecee830b485657af0c50
rjvb7424 <rjvb7424@leeds.ac.uk>
2025-11-29 21:13:09 +0000

    Clean up the code for the personnel file

--- personnel.py
+++ personnel.py
@@ -2,13 +2,13 @@ import random
 import json
 
 class Personnel:
-    with open("nationalities.json", "r", encoding="utf-8") as f:
+    with open("nationalities.json", "r") as f:
         _nationalities = json.load(f)
 
-    with open("names.json", "r", encoding="utf-8") as f:
+    with open("names.json", "r") as f:
         _names = json.load(f)
 
-    with open("positions.json", "r", encoding="utf-8") as f:
+    with open("positions.json", "r") as f:
         _positions = json.load(f)
 
     _all_attributes = [
@@ -35,35 +35,30 @@ class Personnel:
     def __init__(self):
         # personnel identity
         self.gender = random.choice(["male", "female"])
-
         # personnel nationality
         self.nationality = random.choice(list(self._nationalities.keys()))
-        nat_info = self._nationalities[self.nationality]
-
         # pick a first language from the nationality's language list
-        self.first_language = random.choice(nat_info["languages"])
-
+        self.first_language = random.choice(self._nationalities[self.nationality]["languages"])
         # generate first and last names based on the personnel's first language
         self.fname = random.choice(self._names["first_names"][self.first_language][self.gender])
         self.lname = random.choice(self._names["last_names"][self.first_language])
-
-        # build the flag path from the *country* field
-        # e.g. "United States" -> "flags/Flag_of_United_States.png"
-        country = nat_info["country"]
+        # build the flag path from the country field
+        country = self._nationalities[self.nationality]["country"]
         country_for_file = country.replace(" ", "_")
         self.flag_path = f"flags/Flag_of_{country_for_file}.png"
 
-        # personnel position
+        # personnel position and attributes
         self.position = random.choice(list(self._positions.keys()))
-
         # get the primary and secondary attributes for this position
         primary_attrs = set(self._positions[self.position].get("primary", []))
         secondary_attrs = set(self._positions[self.position].get("secondary", []))
-
         # attribute means (tweak these to change “role weight”)
-        PRIMARY_MU = 12   # primary stats tend to be higher
-        SECONDARY_MU = 10 # secondary stats are slightly above average
-        OTHER_MU = 8      # everything else is baseline
+        # primary stats tend to be higher
+        PRIMARY_MU = 12
+        # secondary stats are slightly above average 
+        SECONDARY_MU = 10
+        # everything else is baseline
+        OTHER_MU = 8
 
         self.attributes = {}
 
@@ -77,7 +72,4 @@ class Personnel:
             self.attributes[attr] = self._generate_attribute(mu=mu)
 
     def __repr__(self):
-        return (
-            f"{self.fname} {self.lname} "
-            f"({self.gender}, {self.nationality}, {self.first_language}, {self.position})"
-        )
+        return f"{self.fname} {self.lname}"
